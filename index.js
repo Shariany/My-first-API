35caf3194875b1f2ead8c018e53f39ce85b3712a
Derick Mutezimana <mutez@SI-OPT013.ict.innovationhub.ug>
2025-09-18 09:53:14 +0300

    add responses

--- index.js
+++ index.js
@@ -8,6 +8,41 @@ app.get('/',(request, response) => {
     response.send('Welcome to my backend server!');
 });
 
+//https methods : GET, POST, PUT, DELETE
+
+//routes
+//localhost:5000/
+
+//get request
+app.get('/about', (req, res) => {
+    response.send('This is the about page');
+});
+app.get('/contact', (req, res) => {
+    response.send('This is the contact page');
+});
+
+//Route parameters
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send('User ID is: ' + userId);
+});
+
+//Query parameters
+app.get('/search', (req, res) => {
+    const query = req.query.q;
+    res.send('You searched for: ' + query);
+})
+
+//JSON response
+app.get('products', (req, res) => {
+    const products = [
+        { id: 1, name: 'product 1', price: 100 },
+        { id: 2, name: 'product 2', price: 200 },
+        { id: 3, name: 'product 3', price: 300 },
+    ];
+    res.json(products)
+})
+
 app.listen(PORT, ()=> {
     console.log("Server is running on port" + PORT);
 })
